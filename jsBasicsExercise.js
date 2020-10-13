e2167d747432ed20d6ed5d493adbce5b54dbac96
Todd Simpson <toddsimpson@BCs-MacBook-Pro.local>
2020-10-13 09:54:49 -0500

    Completed Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,45 @@
+
+
+//Number, Booleans, Null, Undefined, String
+
+//+ - * / %
+
+console.log(14);
+
+console.log(7%4);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Spider" + "Man");
+
+console.log("Todd" .length + "Simpson" .length);
+
+console.log("Avengers: Endgame"[16]);
+
+console.log("\\");
+
+console.log(5 ** 4 - 9);
+
+console.log("I" .length - "Am" .length - "Iron" .length - "Man" .length);
+
+console.log(("I" .length + "Am" .length * "Iron" .length) % "Man" .length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR")[3];
+console.log("STARLORD"[0]);
+
+var m = 10;
+var c = 5;
+var u = m = c;
+console.log(u);
+
+
+
+
+
+
